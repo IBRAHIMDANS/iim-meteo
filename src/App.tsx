3dec0e81def31be9c2e06a1c844b5b2693f238fc
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2020-10-07 13:58:55 +0200

    add weather component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./app.css";
+import { Weather } from "./components";
 
 function App() {
   return (
     <div className="app">
-
+      <Weather />
     </div>
   );
 }
