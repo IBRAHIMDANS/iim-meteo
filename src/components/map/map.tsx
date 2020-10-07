7dcadeb43b4db6e54c9d04d168c715aadd00e52d
dupasj <dupasj97@gmail.com>
2020-10-07 17:23:16 +0200

    update

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import axios from "axios";
-import {GeoJSON, Map, TileLayer} from "react-leaflet";
+import L from "leaflet";
 import Cities from "./cities";
+
+/*
+<Map id="map" center={[this.state.lat,this.state.long]} zoom={5} className={"map"}>
+    <TileLayer url="https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png"/>
+    {this.renderSelection()}
+</Map>
+ */
 
 type State = {
     select: any|null,
@@ -19,11 +26,12 @@ type Props = {
 }
 
 class WeatherMap extends React.Component<Props, State>{
+    map: null|L.Map = null;
+    cancel_search: null|(() => void) = null;
+    geojson: null|L.GeoJSON = null;
+
     constructor(props: any) {
         super(props);
-
-        // @ts-ignore
-        this.cancel_search = null;
 
         this.state = {
             lat: 48.8589507,
@@ -50,7 +58,8 @@ class WeatherMap extends React.Component<Props, State>{
             return null;
         }
 
-        return <GeoJSON id="geojson" style={this.geoJSONStyle} data={this.state.select.geojson}/>;
+        return null;
+        //return <GeoJSON id="geojson" style={this.geoJSONStyle} data={this.state.select.geojson}/>;
     }
 
     render() {
@@ -62,10 +71,7 @@ class WeatherMap extends React.Component<Props, State>{
 
         return (
             <div className={"app"}>
-                <Map id="map" center={[this.state.lat,this.state.long]} zoom={5} className={"map"}>
-                    <TileLayer url="https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png"/>
-                    {this.renderSelection()}
-                </Map>
+                <div id={"map"} className={"map"}></div>
                 <div className={classnames.join(" ")}>
                     <form className={"search-container"}>
                         <input placeholder={"Rechercher une ville"} type={"search"} onBlur={() => this.blur()} onFocus={() => this.focus()} value={this.state.search} onChange={(evt) => this.updateSearch(evt)}/>
@@ -76,9 +82,21 @@ class WeatherMap extends React.Component<Props, State>{
         )
     }
 
+    componentDidUpdate(props: Props, state: State) {
+        if (this.state.long !== state.long || this.state.lat !== state.lat && this.map !== null){
+            this.map!.setView([this.state.lat,this.state.long], 13);
+        }
+    }
+
     componentDidMount() {
+        const map = L.map("map").setView([this.state.lat,this.state.long], 13);
+
+        L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
         // @ts-ignore
-       // this.map = L.map("map");
+        this.map = map;
     }
 
     blur(){
@@ -132,7 +150,7 @@ class WeatherMap extends React.Component<Props, State>{
         })
 
         // @ts-ignore
-        const result = await axios.get("https://nominatim.openstreetmap.org/search?format=json&polygon_geojson=1&city="+encodeURI(evt.target.value), {
+        const result = await axios.get("https://nominatim.openstreetmap.org/search?format=json&polygon_geojson=1&q="+encodeURI(evt.target.value), {
             cancelToken: new axios.CancelToken((cancel_search) => {
                 // @ts-ignore
                 this.cancel_search = cancel_search;
@@ -161,12 +179,34 @@ class WeatherMap extends React.Component<Props, State>{
     }
 
     select(information: any){
-        console.log(information);
         this.setState({
             select: information,
             long: Number(information.lon),
             lat: Number(information.lat),
         })
+
+        if (this.geojson !== null){
+            this.geojson.remove();
+            this.geojson = null;
+        }
+
+        if (this.map !== null){
+            this.geojson = new L.GeoJSON(information.geojson,{
+                style: this.geoJSONStyle
+            });
+
+            this.geojson.addTo(this.map);
+
+            const converted = information.boundingbox.map((i: any) => Number(i));
+
+            const bounds = [
+                [converted[0],converted[1]],
+                [converted[2],converted[3]],
+            ]
+
+            //@ts-ignore
+            this.map.fitBounds(bounds);
+        }
 
         return this;
     }
