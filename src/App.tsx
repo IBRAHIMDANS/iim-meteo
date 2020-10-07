192c2fe1db7306e41ef6aa7acb931c9efa34cffb
dupasj <dupasj97@gmail.com>
2020-10-07 14:10:41 +0200

    add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./app.css";
+import { Map, TileLayer } from 'react-leaflet'
 
 function App() {
   return (
     <div className="app">
-
+      <Map center={[48.8589507,2.2770205]} zoom={19} className={"map"}>
+        <TileLayer url="https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png"/>
+      </Map>
     </div>
   );
 }
